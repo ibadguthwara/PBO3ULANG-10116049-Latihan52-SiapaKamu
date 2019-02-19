d9fc1a6e70e4277713984b969e52fa808f7b06d0
M Ibad Guthwara <43487906+ibadguthwara@users.noreply.github.com>
2019-02-19 20:52:04 +0700

    Add files via upload

---
+++ PBO3ULANG-10116049-Latihan52-SiapaKamu/src/pbo3ulang/pkg10116049/latihan52/siapakamu/PBO3ULANG10116049Latihan52SiapaKamu.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3ulang.pkg10116049.latihan52.siapakamu;
+
+/**
+ *
+ * @author ibadguthwara
+ * NAMA              : M. Ibad Guthwara
+ * KELAS             : PBO 3 ULANG
+ * NIM               : 10116049
+ * DESKRIPSI PROGRAM : 
+ */
+public class PBO3ULANG10116049Latihan52SiapaKamu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Dosen dosen = new Dosen();
+        dosen.setNip("41227829930");
+        dosen.setNama("Rizki Adam Kurniawan");
+        dosen.setUmur(27);
+        dosen.setMataKuliah("PBO");
+
+        System.out.println("NIP DOSEN : "+dosen.getNip());
+        dosen.siapaKamu();
+        dosen.mengajarApa();
+
+        Mahasiswa mahasiswa = new Mahasiswa();
+        mahasiswa.setNim("10116049");
+        mahasiswa.setNama("M. Ibad Guthwara");
+        mahasiswa.setUmur(20);
+        mahasiswa.setKelas("PBO3ULANG");
+
+        System.out.println("\nNIP MAHASISWA : "+mahasiswa.getNim());
+        mahasiswa.siapaKamu();
+        mahasiswa.kelasApa();
+    }
+    
+}
